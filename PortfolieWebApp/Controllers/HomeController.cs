fb136148546400e64e3d48ff358459dbd0afd23a
Ben Rose <bcrose@bsu.edu>
2021-01-23 12:58:18 -0500

    Add project files.

---
+++ PortfolieWebApp/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolieWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "My Current Job Resume";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Ben Rose";
+
+            return View();
+        }
+    }
+}
